8f9caf3865ab298f59de2a2c08e5bad9964b3f75
QBIFV2 <binhovescovi@gmail.com>
2025-04-23 16:27:08 -0300

    Update form.tsx

--- components/ui/form.tsx
+++ components/ui/form.tsx
@@ -1,178 +1,93 @@
 "use client"
 
 import * as React from "react"
-import * as LabelPrimitive from "@radix-ui/react-label"
-import { Slot } from "@radix-ui/react-slot"
-import {
-  Controller,
-  ControllerProps,
-  FieldPath,
-  FieldValues,
-  FormProvider,
-  useFormContext,
-} from "react-hook-form"
+import { useForm } from "react-hook-form"
 
-import { cn } from "@/lib/utils"
-import { Label } from "@/components/ui/label"
+import { Form, FormItem, FormLabel, FormControl, FormDescription, FormMessage, FormField } from "@/components/Form"
 
-const Form = FormProvider
+const ContactForm = () => {
+  const { handleSubmit, control } = useForm()
 
-type FormFieldContextValue<
-  TFieldValues extends FieldValues = FieldValues,
-  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
-> = {
-  name: TName
-}
-
-const FormFieldContext = React.createContext<FormFieldContextValue>(
-  {} as FormFieldContextValue
-)
-
-const FormField = <
-  TFieldValues extends FieldValues = FieldValues,
-  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
->({
-  ...props
-}: ControllerProps<TFieldValues, TName>) => {
-  return (
-    <FormFieldContext.Provider value={{ name: props.name }}>
-      <Controller {...props} />
-    </FormFieldContext.Provider>
-  )
-}
-
-const useFormField = () => {
-  const fieldContext = React.useContext(FormFieldContext)
-  const itemContext = React.useContext(FormItemContext)
-  const { getFieldState, formState } = useFormContext()
-
-  const fieldState = getFieldState(fieldContext.name, formState)
-
-  if (!fieldContext) {
-    throw new Error("useFormField should be used within <FormField>")
-  }
-
-  const { id } = itemContext
-
-  return {
-    id,
-    name: fieldContext.name,
-    formItemId: `${id}-form-item`,
-    formDescriptionId: `${id}-form-item-description`,
-    formMessageId: `${id}-form-item-message`,
-    ...fieldState,
-  }
-}
-
-type FormItemContextValue = {
-  id: string
-}
-
-const FormItemContext = React.createContext<FormItemContextValue>(
-  {} as FormItemContextValue
-)
-
-const FormItem = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => {
-  const id = React.useId()
-
-  return (
-    <FormItemContext.Provider value={{ id }}>
-      <div ref={ref} className={cn("space-y-2", className)} {...props} />
-    </FormItemContext.Provider>
-  )
-})
-FormItem.displayName = "FormItem"
-
-const FormLabel = React.forwardRef<
-  React.ElementRef<typeof LabelPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>
->(({ className, ...props }, ref) => {
-  const { error, formItemId } = useFormField()
-
-  return (
-    <Label
-      ref={ref}
-      className={cn(error && "text-destructive", className)}
-      htmlFor={formItemId}
-      {...props}
-    />
-  )
-})
-FormLabel.displayName = "FormLabel"
-
-const FormControl = React.forwardRef<
-  React.ElementRef<typeof Slot>,
-  React.ComponentPropsWithoutRef<typeof Slot>
->(({ ...props }, ref) => {
-  const { error, formItemId, formDescriptionId, formMessageId } = useFormField()
-
-  return (
-    <Slot
-      ref={ref}
-      id={formItemId}
-      aria-describedby={
-        !error
-          ? `${formDescriptionId}`
-          : `${formDescriptionId} ${formMessageId}`
-      }
-      aria-invalid={!!error}
-      {...props}
-    />
-  )
-})
-FormControl.displayName = "FormControl"
-
-const FormDescription = React.forwardRef<
-  HTMLParagraphElement,
-  React.HTMLAttributes<HTMLParagraphElement>
->(({ className, ...props }, ref) => {
-  const { formDescriptionId } = useFormField()
-
-  return (
-    <p
-      ref={ref}
-      id={formDescriptionId}
-      className={cn("text-sm text-muted-foreground", className)}
-      {...props}
-    />
-  )
-})
-FormDescription.displayName = "FormDescription"
-
-const FormMessage = React.forwardRef<
-  HTMLParagraphElement,
-  React.HTMLAttributes<HTMLParagraphElement>
->(({ className, children, ...props }, ref) => {
-  const { error, formMessageId } = useFormField()
-  const body = error ? String(error?.message) : children
-
-  if (!body) {
-    return null
+  const onSubmit = (data: any) => {
+    // O formulário será enviado automaticamente para Formsubmit
+    // Não é necessário fazer mais nada aqui, pois o envio é tratado no action do form
+    console.log(data)
   }
 
   return (
-    <p
-      ref={ref}
-      id={formMessageId}
-      className={cn("text-sm font-medium text-destructive", className)}
-      {...props}
-    >
-      {body}
-    </p>
-  )
-})
-FormMessage.displayName = "FormMessage"
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <form
+        action="https://formsubmit.co/qbistudioai@gmail.com"
+        method="POST"
+        className="space-y-4"
+      >
+        {/* Nome */}
+        <FormField name="name" control={control} rules={{ required: "Nome é obrigatório" }}>
+          <FormItem>
+            <FormLabel htmlFor="name">Nome</FormLabel>
+            <FormControl>
+              <input
+                id="name"
+                name="name"
+                placeholder="Seu nome"
+                className="input"
+                required
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        </FormField>
 
-export {
-  useFormField,
-  Form,
-  FormItem,
-  FormLabel,
-  FormControl,
-  FormDescription,
-  FormMessage,
-  FormField,
+        {/* Email */}
+        <FormField name="email" control={control} rules={{ required: "Email é obrigatório" }}>
+          <FormItem>
+            <FormLabel htmlFor="email">Email</FormLabel>
+            <FormControl>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="Seu email"
+                className="input"
+                required
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        </FormField>
+
+        {/* Mensagem */}
+        <FormField name="message" control={control} rules={{ required: "Mensagem é obrigatória" }}>
+          <FormItem>
+            <FormLabel htmlFor="message">Mensagem</FormLabel>
+            <FormControl>
+              <textarea
+                id="message"
+                name="message"
+                placeholder="Sua mensagem"
+                className="input"
+                required
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        </FormField>
+
+        {/* Campos Ocultos para Formsubmit */}
+        <input type="hidden" name="_captcha" value="false" />
+        <input
+          type="hidden"
+          name="_next"
+          value="https://qbifv2.github.io/meu-site-v0/obrigado.html"
+        />
+
+        {/* Botão de Envio */}
+        <button type="submit" className="btn">
+          Enviar
+        </button>
+      </form>
+    </Form>
+  )
 }
+
+export default ContactForm
